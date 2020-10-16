50746d8c85d8aa3404340e37fd358a2c2d12d3c8
Josiah <mcflyhalf@live.com>
2020-10-16 16:08:40 +0400

    Set up skeleton of Gym and Workout classes. For real this time

---
+++ teme/edu.py
@@ -0,0 +1,44 @@
+from teme import get_logger
+
+logger = get_logger("teme_workout") 
+
+
+class Workout:
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+	#TotalPoints
+	def get_points(self):
+		pass
+
+
+
+
+
+class Gym:
+	def __init__(self):
+		pass
+
+
+	def add_workout(self):
+		pass
+
+	def remove_workout(self):
+		pass
+
+	def add_instructor(self):
+		pass
+
+	def remove_instructor(self):
+		pass
+
+	def add_student(self):
+		pass
+
+	def remove_student(self):
+		pass
+
+
